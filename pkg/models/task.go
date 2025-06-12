ecb6db551abd58b2263e196ce310ced02d757a0e
Rukshan <it19129204@my.sliit.lk>
2025-06-12 23:35:33 +0530

    Update task.go

--- pkg/models/task.go
+++ pkg/models/task.go
@@ -3,6 +3,6 @@ package models
 type Task struct {
 	ID          int    `json:"id"`          // Unique identifier for the task
 	Description string `json:"description"` // Task description (mandatory)
-	Status      string `json:"status"`      // Task status (e.g., "pending", "completed")
+	Status      string `json:"status"`      // Task status (e.g., "pending", "in progress", "completed")
 	Priority    int    `json:"priority"`    // Task priority (integer between 1 and 3)
 }
